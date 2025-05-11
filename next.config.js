d32b02576f866435a0c1db0e9f900cdbd8ef91e8
liachev <2000stan@gmail.com>
2025-05-10 23:27:20 -0400

    Delete next.config.js

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  // This ensures our static HTML files are properly served
-  trailingSlash: true
-};
-
-module.exports = nextConfig;
